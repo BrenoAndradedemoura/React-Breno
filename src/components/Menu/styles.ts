a5f808bed54da7c837a4c4ce4f675ab967b1e19e
BrenoAndradedemoura <brenomourasandrade@gmail.com>
2022-10-14 08:41:50 -0300

    tudo certo

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -2,30 +2,39 @@ import styled from "styled-components"
 import { colors } from "styles/GlobalStyle"
 
 export const Cabecalho = styled.header`
+display: flex;
+border-bottom: 0.5rem solid ${colors.secondary};
+picture svg {
+  height: 6rem;
+  width: 6rem;
+}
+nav {
+  width: 100%;
   display: flex;
-  background-color: ${colors.primary};
-  border-bottom: 1.8rem solid ${colors.third};
-  picture svg {
-    height: 3rem;
-    width: 3rem;
-  }
-  nav {
-    width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  ul {
+    list-style-type: none;
     display: flex;
-    justify-content: flex-end;
     align-items: center;
-    ul {
-      list-style-type: none;
-      display: flex;
-      align-items: center;
-      li {
-        margin: 0 1.5em;
-        a {
-          text-decoration: none;
-          font-size: 1.5em;
-          color: ${colors.white};
-        }
+    li {
+      margin: 0 1.5em;
+      a {
+        text-decoration: none;
+        font-size: 1.2em;
+        color: ${colors.primary};
       }
-    }
-  }
-`
+      button {
+        display: flex;
+        gap: 0.5rem;
+        align-items: center;
+        text-decoration: none;
+        font-size: 1.2em;
+        color: ${colors.primary};
+        border: 0;
+        background-color: transparent;
+        cursor: pointer;
+       }
+     }
+   }
+ }`
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -14,16 +14,47 @@ import { AxiosError } from "axios";
 
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { signIn } = useAuth()
+  const [formData, setFormData] = useState<IUser>({
+    email: '',
+    password: '',
+  })
+  async function handleChange(e: IUser) {
+    setFormData((state: IUser | undefined) => ({ ...state, ...e }))
+  }
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    try {
+      const { email, password } = formData
+      await signIn({
+        email: String(email),
+        password: String(password),
+      })
+      toast.success("Login realizado com sucesso!");
+      navigate('/adm')
+    } catch (error) {
+      const err = error as AxiosError<IErrorResponse>
+      toast.error(String(err.response?.data))
+    }
+  }
+
   return (
+    
     <S.Section>
       <form action="cadastrar.html" method="post">
         <label htmlFor="email">E-mail</label>
         <div>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" 
+          onChange={(e) => handleChange({email: e.target.value})}
+          value={formData?.email} 
+          />
         </div>
         <label htmlFor="senha">Senha</label>
         <div>
-          <input type="password" name="senha" id="senha" />
+          <input type="password" name="senha" id="senha"
+          onChange={(e) => handleChange({password: e.target.value})}
+          />
         </div>
         <p>
           Não possui conta? <Link to="/cadastrar">Cadastre-se</Link>
